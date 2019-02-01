fd7422fa04b6dc7971f8aa3a93f47d2d463cc91d
cjrojas72 <chris@Christians-MacBook-Pro.local>
2019-02-01 12:29:31 -0500

    update to file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,17 +2,19 @@
 const inquirer = require("inquirer");
 const connection = require("./connection");
 
+// tests connection to database
 connection.connect(function(err) {
   console.log("conecting......");
   if (err){ 
     throw err;
   }
-  // run the start function after the connection is made to prompt the user
+  // run the startApp function after the connection is made to prompt the user
   console.log("Connected!");
   startApp();
 });
 
 
+//prompts user if they would like to view proudcts in inventory or buy a product
 function startApp(){
   inquirer.prompt({
     name: "greetings",
@@ -33,6 +35,8 @@ function startApp(){
   });
 }
 
+
+//displays items in inventory
 function displayItems(){
   connection.query("SELECT * FROM products", function(err, results) {
     if(err) throw err;
@@ -41,10 +45,12 @@ function displayItems(){
   });
 }
 
+//starts process to buy a product
 function buyProduct() {
   connection.query("SELECT * FROM products", function(err, results) {
     if(err) throw err;
 
+  //lists products and asks user to select product and how many they want
   inquirer
     .prompt([
       {
@@ -67,7 +73,9 @@ function buyProduct() {
     ])
     .then(function(answer) {
       
+
       var item;
+      //searches for product in database
       for(var i=0; i< results.length; i++){
         if(results[i].product_name === answer.item){
           item = results[i];
@@ -76,9 +84,10 @@ function buyProduct() {
       console.log("You chose " + item.product_name);
       console.log("We have " + item.stock_quantity + " in stock");
 
-
+      //if amount desired is in stock then update database and give a price for user
       if(item.stock_quantity > parseInt(answer.amount)){
 
+        //holds data to update into database and display price for user
         var newInventory = item.stock_quantity - answer.amount;
         var totalPrice = item.price * answer.amount;
 
@@ -91,6 +100,7 @@ function buyProduct() {
           item_id: item.item_id
           }
         ],
+        //displays proudct info for user
         function(error){
           if(error) throw err;
           console.log("You would like to buy " + answer.amount +" "+item.product_name +" at $"+item.price + " each");
@@ -98,6 +108,7 @@ function buyProduct() {
           startApp();
         });
       }
+      //if amount desired is not in stock then let user know
       else{
         console.log("sorry we don't have that much you requested in stock");
         startApp();
